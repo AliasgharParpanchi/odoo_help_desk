f3e97904778b93461dc9df5348db462a1c6e8284
AliasgharParpanchi <aliasghar.parpanchi@yahoo.com>
2026-06-15 22:44:50 +0330

    change manifest and create init

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Help Desk Custom',
+    'version': '0.1',
+    'author': 'Aliasghar',
+    'depends': ['base', 'mail', 'calendar', 'contacts'],
+    'category': 'Services/Helpdesk',
+    'summary': "Helpdesk ticketing with priorities, assignments, and PDF reports",
+
+    'installable': True,
+    'application': True,
+}
